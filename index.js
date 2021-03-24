8bbd04772e1d4bd06fbcfee9e3d4ba8300d92162
Adrián Villar <adrian@dekalabs.com>
2021-03-24 13:16:53 +0100

    probe rabbit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const moment = require('moment')
+const client = require('@xtreamr/ds_rabbitmq').client
 
 function main() {
   const pruebaDate = moment().format('DD MM YYYY, h:mm:ss a');
